fd0d936baf7e8389c77252687e0c1552c536dc5d
Neil <johnnycocheroo454@gmail.com>
2020-10-22 20:25:02 +0100

    Showsite page amends

--- showsite.php
+++ showsite.php
@@ -2,17 +2,8 @@
 <?php get_header(); ?>
 
 	<main class="__showsite main">
-						
-		<div class="showsite-text">
-			<div class="showsite"><?php the_field( 'showsite_text' ); ?></div>
-			<div class="viewing-times-container">
-				<div class="viewing-times"><?php the_field( 'viewing_times' ); ?></div>
-			</div>
-		</div>
-		<?php
-			//$swindon_showsite = get_field( 'swindon_showsite' );
-			$high_beeches_site = get_field( 'high_beeches_site' );
-		?>
+		
+		
 		<div id="swindon-showsite" class="showsite-image">
 			<video width="100%" height="100%" poster="/wp-content/uploads/showsite.jpg" controls="show" class="showsite-video">
 				<source src="/wp-content/uploads/primeoak.webm" type="video/webm ">
@@ -25,13 +16,26 @@
 			<?= the_field( 'swindon_showsite_text' ); ?>
 		</div>
 
-		<div id="high-beeches-showsite-text" class="showsite-products">
-			<?= the_field( 'high_beeches_site_text' ); ?>
-		</div>
-		
+		<?php
+			$high_beeches_site = get_field( 'high_beeches_site' );
+		?>
+
 		<div id="high-beeches-showsite" class="showsite-image">
 			<img src="<?=$high_beeches_site['url']; ?>" alt="<?=$high_beeches_site['alt']; ?>">
 		</div>
+
+		<div id="high-beeches-showsite-text" class="showsite-products">
+			<?= the_field( 'high_beeches_site_text' ); ?>
+		</div>
+
+		
+			<div class="showsite-text">
+				<div class="showsite"><?php the_field( 'showsite_text' ); ?></div>
+				<div class="viewing-times-container">
+					<div class="viewing-times"><?php the_field( 'viewing_times' ); ?></div>
+				</div>
+			</div>	
+
 		<div class="showsite-drone-video centred">
 			<div class="drone-video-inner">
 				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/wc7GVnFv1x0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
