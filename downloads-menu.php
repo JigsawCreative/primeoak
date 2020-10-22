46739edb97a6831abfbeda9bc8a5ef4bb7524ca4
Neil <johnnycocheroo454@gmail.com>
2020-10-22 11:02:44 +0100

    Mobile downloads menu put in place.

---
+++ downloads-menu.php
@@ -0,0 +1,10 @@
+<?php /* Template Name: Downloads Menu */ ?>
+<?php get_header(); ?>
+
+	<main class="__downloads">
+		<div class="downloads-list">
+			<?php downloads_nav(); ?>
+		</div>
+	</main>
+
+<?php get_footer(); ?>
