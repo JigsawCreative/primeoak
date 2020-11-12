a219589d08cdfc76508ff1f1539df04ecff21d44
Neil Williams <n.williams@primeoak.co.uk>
2020-11-12 09:48:06 +0000

    Deleted dist/css recovered and video TL tweaked

--- dist/js/front-page-video.js
+++ dist/js/front-page-video.js
@@ -5,17 +5,14 @@ const logo = document.querySelector('.prime-oak-logo');
 function videoIntro() {
     console.log('front page called');
     const homepageTl = gsap.timeline({
-        defaults: {duration: 2, ease: "power2.inOut"}
+        defaults: {duration: 1.5, ease: "power2.inOut"}
     });
-    homepageTl.fromTo(videoTitle, {opacity: "0"}, {opacity: "1"});
-    homepageTl.fromTo(videoTitle, {opacity: "1"}, {opacity: "0"});
-    homepageTl.to(videoTitle, {display: "none"});
+    homepageTl.to(videoTitle, {opacity: "1"});
+    homepageTl.to(videoTitle, {opacity: "0", display: "none"});
     homepageTl.to(logo, {opacity: "1"});
-    homepageTl.to(logo, {opacity: "0"});
-    homepageTl.to(logo, {display: "none"});
+    homepageTl.to(logo, {opacity: "0", display: "none"});
     homepageTl.to(videoTitleBox, {display: "none"});
-    homepageTl.fromTo(videoContainer, {opacity:"1"}, {duration: "1", opacity: "0"});
-    homepageTl.to(videoContainer, {display: "none"});
+    homepageTl.to(videoContainer, {opacity: "0", display: "none"});
 }
 
 videoIntro();
